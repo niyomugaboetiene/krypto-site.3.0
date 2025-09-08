5875dd1551143fb5c26ed5ad204d905025b374a8
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 13:40:47 -0700

    Initial commit of kryptosite.3.0

---
+++ krypt-site-frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+
+const Dashboard = () => {
+    return (
+        <div
+          className="min-h-screen bg-gradient-to-r from-blue-800 via-gray-500 to-purple-600"
+        >
+
+          </div>
+    )
+}
+
+export default Dashboard;

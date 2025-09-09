b5572b068353930a01d1914d15952418234f4fb4
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 18:33:24 -0700

    Initial commit of kryptosite.3.0

--- krypt-site-frontend/src/pages/Dashboard.tsx
+++ krypt-site-frontend/src/pages/Dashboard.tsx
@@ -3,13 +3,13 @@ import frontend from "../../public/frontend.gif";
 const Dashboard = () => {
     return (
         <div className="min-h-screen">
-            <div className="">
-              <aside>
+            <div className="flex justify-between">
+              <aside className="max-w-md">
                 Crypto Site is modern decentralized application allows easy seeling and receiving token
                 it is flexible and simple. <span>Its time to go !!</span>
               </aside>
 
-              <aside>
+              <aside className="max-w-md">
                   <img src={frontend} />
               </aside>
             </div>

5b538cfd042955b4a5ede8817fe36460733d205d
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 18:48:18 -0700

    Initial commit of kryptosite.3.0

--- krypt-site-frontend/src/pages/Dashboard.tsx
+++ krypt-site-frontend/src/pages/Dashboard.tsx
@@ -8,8 +8,8 @@ const Dashboard = () => {
                 Welcome to <span className="font-extrabold bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-500 bg-clip-text text-transparent drop-shadow-lg mb-4 md:mb-0">Crypto Site</span>, 
                 your go-to platform for exploring the world of blockchain and digital assets. Here, you can track your tokens, manage your wallet, interact with NFTs,
                 and monitor transactions — all in one secure and user-friendly interface. <br /><br />
-                Crypto Site is modern decentralized application allows easy seeling and receiving token
-                it is flexible and simple you can exprole crypto world. Buy and sell usng this moder app. <span>Its time to go !!</span>
+                <span className="font-extrabold bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-500 bg-clip-text text-transparent drop-shadow-lg mb-4 md:mb-0">Crypto Site</span> is modern decentralized application allows easy seeling and receiving token
+                it is flexible and simple you can exprole crypto world. Buy and sell usng this moder app. <span className="italic">Its time to go !!</span>
               </aside>
 
               <aside className="max-w-md">

080fb272f9be00511ec30aa4d557bfc15af6fb0c
nietiene <niyomugaboetiene53@gmail.com>
2025-09-08 17:30:28 -0700

    Initial commit of kryptosite.3.0

--- krypt-site-frontend/src/pages/menu.tsx
+++ krypt-site-frontend/src/pages/menu.tsx
@@ -1,13 +1,16 @@
 
-
 const Menu = () => {
     return (
         <div
            className="min-h-screen bg-gradient-to-r from-blue-800 via-gray-500 to-purple-600"
            >
-             <div className="flex text-center items-center shadow-lg">
+                <h1 className="font-extrabold text-2xl text-left">
+                 KRYPTO SITE
+              </h1>
+             <div className="flex text-center justify-center shadow-lg mb-4 font-bold text-white space-x-10 ">
                 <p>Home</p>
                 <p>Wallet</p>
+                <p>About Us</p>
                 <p>Contact Us</p>
              </div>
            </div>

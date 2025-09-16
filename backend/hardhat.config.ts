54c26c92315d6c5bb6688b2f68cb89ec3541f8be
nietiene <niyomugaboetiene53@gmail.com>
2025-09-15 18:19:54 -0700

    bug fix

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,5 +1,5 @@
 import "@nomicfoundation/hardhat-toolbox";
-import { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",

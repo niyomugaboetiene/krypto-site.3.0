ae6bda2c0ed5b546dc4d074ad44974b05f2c20d9
nietiene <niyomugaboetiene53@gmail.com>
2025-09-16 07:30:56 -0700

    Update package

--- backend/scripts/dao.ts
+++ backend/scripts/dao.ts
@@ -1,4 +1,13 @@
 import { ethers } from "hardhat";
 export async function main() {
-    const factory = await ethers.
+    const factory = await ethers.getContractFactory("KryptoDAO");
+    const kryptodao = await factory.deploy();
+
+    await kryptodao.waitForDeployment();
+    const address = await kryptodao.getAddress();
+    console.log("Address:", address);
 }
+
+main().catch((error) => {
+    console.error("ERROR:", error);
+});

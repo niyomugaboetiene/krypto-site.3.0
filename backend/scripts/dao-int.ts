87159c746aa4b77d65a87948c651dd8f4822de12
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-09-24 08:37:27 +0200

    end fixing about DAO backend

--- backend/scripts/dao-int.ts
+++ backend/scripts/dao-int.ts
@@ -2,16 +2,18 @@ import { ethers } from "hardhat";
 import { KryptoDAO } from "../typechain-types";
 
 export async function main() {
-    const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
     const factory = await ethers.getContractFactory("KryptoDAO");
     const kryptodao = factory.attach(address) as KryptoDAO;
 
-    // const createProposal = await kryptodao.CreateProposal("Testing proposal", "This proposal contain testing one");
-    // createProposal.wait();
-    // console.log("Proposal created");
+    const createProposal = await kryptodao.CreateProposal("Testing proposal", "This proposal contain testing one");
+    createProposal.wait();
+    console.log("Proposal created");
 
-    const getProposal = await kryptodao.GetProposal();
-    console.log("Proposals:", getProposal);
+    const [_name, description, voteCount, executed ] = await kryptodao.GetProposal();
+    for (let i = 0; i < _name.length; i ++) {
+        console.log(_name[i], description[i], voteCount[i], executed[i]);
+    }
 }
 
 main().catch((error) => {

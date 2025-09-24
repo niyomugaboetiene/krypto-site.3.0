a12bbec0b79d86b9b667201a893b8f980c38b909
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-09-24 10:13:12 +0200

    start fixing backend

--- backend/scripts/dao-int.ts
+++ backend/scripts/dao-int.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { KryptoDAO } from "../typechain-types";
 
 export async function main() {
-    const address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+    const address = "0x8b9A24d230997448A46AD58ab2b3c299E6DC74cb";
     const factory = await ethers.getContractFactory("KryptoDAO");
     const kryptodao = factory.attach(address) as KryptoDAO;
 
